99e558a4debf29f060a7a8489e60500d135fe3c6
ASPayne <pay12008@byui.edu>
2017-05-31 13:18:49 -0600

    creating an intentional conflict

--- app/src/main/java/eventswithred/com/redrewards/MainActivity.java
+++ app/src/main/java/eventswithred/com/redrewards/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
     /*we need to add more code here*/
     // I think we can submit it as is
     //are you sure about that?
+    //never has such a time come. time to call "hello world"
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
