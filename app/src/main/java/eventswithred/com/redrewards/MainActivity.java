474c69fbd8c89771a7cfa55dc67931ecd5238ac8
nikhuff <nik.huff@me.com>
2017-05-31 13:13:00 -0600

    Added a comment

--- app/src/main/java/eventswithred/com/redrewards/MainActivity.java
+++ app/src/main/java/eventswithred/com/redrewards/MainActivity.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     /*we need to add more code here*/
+    // I think we can submit it as is
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
