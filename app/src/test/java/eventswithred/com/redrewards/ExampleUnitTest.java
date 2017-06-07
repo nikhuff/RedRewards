23ac0139035750c5ebc46faf98a01c1d6aaa0016
ASPayne <pay12008@byui.edu>
2017-06-07 13:26:12 -0600

    working with debugging/testing

--- app/src/test/java/eventswithred/com/redrewards/ExampleUnitTest.java
+++ app/src/test/java/eventswithred/com/redrewards/ExampleUnitTest.java
@@ -10,8 +10,21 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
+    User USER = new User();
+
     @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+    public void User_Has_A_Name() throws Exception {
+        USER.setUserName("bob");
+        assertNotNull("NULL user name", USER.getUserName());
+    }
+    @Test
+    public void User_Points_Not_negitive() throws Exception {
+        assertTrue(USER.getPoints() >= 0);
+    }
+    @Test
+    public void User_Has_An_ID() throws Exception {
+        USER.setUserID("15a53e2d");
+        assertNotNull("NULL user ID", USER.getUserID());
     }
 }
