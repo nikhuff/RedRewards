fe87eac4b0e7635d4ec86c4971e2a13add46e723
nikhuff <nik.huff@me.com>
2017-06-07 13:15:08 -0600

    added stubbed user class and social accounts interface

---
+++ app/src/main/java/eventswithred/com/redrewards/SocialAccount.java
@@ -0,0 +1,11 @@
+package eventswithred.com.redrewards;
+
+/**
+ * Created by nik on 6/7/17.
+ */
+
+public interface SocialAccount {
+
+    void confirmPosting();
+    void awardPoints();
+}
---
+++ app/src/main/java/eventswithred/com/redrewards/User.java
@@ -0,0 +1,38 @@
+package eventswithred.com.redrewards;
+
+import java.util.List;
+
+/**
+ * Created by nik on 6/7/17.
+ */
+
+public class User {
+    private String userName;
+    private String userID;
+    private int points;
+    public List<SocialAccount> socialAccountList;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+}
