47ea91e4482d991aa615852fa33ba445204aeafb
ASPayne <pay12008@byui.edu>
2017-06-10 18:08:13 -0600

    Created Main Menu Bar

--- app/src/main/java/eventswithred/com/redrewards/MainActivity.java
+++ app/src/main/java/eventswithred/com/redrewards/MainActivity.java
@@ -4,6 +4,10 @@ import android.app.Activity;
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.util.Log;
 import android.widget.Button;
 import android.widget.TabHost;
@@ -14,6 +18,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar top_menu_bar = (Toolbar) findViewById(R.id.main_toolbar);
+        setSupportActionBar(top_menu_bar);
+        ActionBar ab = getSupportActionBar();
     }
 
+    // Menu icons are inflated just as they were with actionbar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.top_menu_bar, menu);
+        return true;
+    }
 }
