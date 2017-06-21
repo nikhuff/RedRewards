f9edfd61b4b166ce3f6237cd278fdd7ebeadee15
nikhuff <nik.huff@me.com>
2017-06-21 12:57:36 -0600

    added facebook login to facebook fragment

--- app/src/main/java/eventswithred/com/redrewards/FacebookTabFragment.java
+++ app/src/main/java/eventswithred/com/redrewards/FacebookTabFragment.java
@@ -1,10 +1,18 @@
 package eventswithred.com.redrewards;
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.facebook.AccessToken;
+import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.HttpMethod;
+import com.facebook.appevents.AppEventsLogger;
 
 public class FacebookTabFragment extends Fragment {
 
@@ -12,5 +20,22 @@ public class FacebookTabFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.facebook_tab_fragment, container, false);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        new GraphRequest(
+                AccessToken.getCurrentAccessToken(),
+                "/Revolutionary Events & DJs/feed",
+                null,
+                HttpMethod.GET,
+                new GraphRequest.Callback() {
+                    public void onCompleted(GraphResponse response) {
+                        /* handle the result */
+                    }
+                }
+        ).executeAsync();
     }
 }
