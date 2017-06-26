dde84a9899211c79f255e8d3d89e4e38d08a38e5
ASPayne <pay12008@byui.edu>
2017-06-26 16:51:15 -0600

    worked on making a list for the store

--- app/src/main/java/eventswithred/com/redrewards/StoreTabFragment.java
+++ app/src/main/java/eventswithred/com/redrewards/StoreTabFragment.java
@@ -1,102 +1,145 @@
 package eventswithred.com.redrewards;
 
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v4.app.ListFragment;
-import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
+import android.view.ViewStub;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ImageView;
-import android.widget.ListAdapter;
+import android.widget.GridView;
 import android.widget.ListView;
-import android.widget.TextView;
 import android.widget.Toast;
-import android.util.Log;
 
 import java.util.ArrayList;
-
-public class StoreTabFragment extends ListFragment implements AdapterView.OnItemClickListener {
-
-   // StoreItem holder = new StoreItem();
-    //holder.itemImage = (ImageView) convertView.findViewById(R.id.listitem_image);
-    //holder.text = (TextView) convertView.findViewById(R.id.listitem_text);
-    //holder.timestamp = (TextView) convertView.findViewById(R.id.listitem_timestamp);
-    //holder.progress = (ProgressBar) convertView.findViewById(R.id.progress_spinner);
-//convertView.setTag(holder);
+import java.util.List;
 
 
-    //working with ListView
-    //https://developer.android.com/training/improving-layouts/smooth-scrolling.html
-    //https://developer.android.com/reference/android/widget/ListView.html
+public class StoreTabFragment extends AppCompatActivity{
+    private ViewStub stubGrid;
+    private ViewStub stubList;
+    private ListView listView;
+    private GridView gridView;
+    private StoreListViewAdapter listViewAdapter;
+    private StoreGridViewAdapter gridViewAdapter;
+    private List<StoreProduct> productList;
+    private int currentViewMode = 0;
 
-    //filling and designing a layout
-    //https://developer.android.com/guide/topics/ui/declaring-layout.html#FillingTheLayout
-
-    //layout class
-    //  https://developer.android.com/reference/android/R.layout.html
-    //  int list_content
-    //  Standard content view for a ListFragment. If you are implementing a subclass of ListFragment
-    // with your own customized content, you can include this layout in that content to still retain
-    // all of the standard functionality of the base class.
-
-
-
-    //SITE THAT PROVIDED CODE TO FINALLY GET A LIST TO SHOW
-    //https://www.tutorialspoint.com/android/android_list_fragment.htm
+    static final int VIEW_MODE_LISTVIEW = 0;
+    static final int VIEW_MODE_GRIDVIEW = 1;
 
     @Override
-    public View onCreateView(LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.store_tab_fragment);
 
-        View view = inflater.inflate(R.layout.store_tab_fragment, container, false);
-        return view;
+        stubList = (ViewStub) findViewById(R.id.stub_list);
+        stubGrid = (ViewStub) findViewById(R.id.stub_grid);
+
+        //Inflate ViewStub before get view
+
+        stubList.inflate();
+        stubGrid.inflate();
+
+        listView = (ListView) findViewById(R.id.mylistview);
+        gridView = (GridView) findViewById(R.id.mygridview);
+
+        //get list of product
+        getProductList();
+
+        //Get current view mode in share reference
+        SharedPreferences sharedPreferences = getSharedPreferences("ViewMode", MODE_PRIVATE);
+        currentViewMode = sharedPreferences.getInt("currentViewMode", VIEW_MODE_LISTVIEW);//Default is view listview
+        //Register item lick
+        listView.setOnItemClickListener(onItemClick);
+        gridView.setOnItemClickListener(onItemClick);
+
+        switchView();
+
+    }
+
+
+    private void switchView() {
+
+        if(VIEW_MODE_LISTVIEW == currentViewMode) {
+            //Display listview
+            stubList.setVisibility(View.VISIBLE);
+            //Hide gridview
+            stubGrid.setVisibility(View.GONE);
+        } else {
+            //Hide listview
+            stubList.setVisibility(View.GONE);
+            //Display gridview
+            stubGrid.setVisibility(View.VISIBLE);
+        }
+        setAdapters();
+    }
+
+    private void setAdapters() {
+        if(VIEW_MODE_LISTVIEW == currentViewMode) {
+            listViewAdapter = new StoreListViewAdapter(this, R.layout.store_list_item, productList);
+            listView.setAdapter(listViewAdapter);
+        } else {
+            gridViewAdapter = new StoreGridViewAdapter(this, R.layout.store_grid_item, productList);
+            gridView.setAdapter(gridViewAdapter);
+        }
+    }
+
+    public List<StoreProduct> getProductList() {
+        //pseudo code to get product, replace your code to get real product here
+        productList = new ArrayList<>();
+        productList.add(new StoreProduct(R.drawable.red2, "RED T-Shirt", "A Standard RED T-Shirt", 10));
+        productList.add(new StoreProduct(R.drawable.red2, "Title 2", "This is description 2", 10));
+        productList.add(new StoreProduct(R.drawable.red2, "Title 3", "This is description 3", 10));
+        productList.add(new StoreProduct(R.drawable.red2, "Title 4", "This is description 4", 10));
+        productList.add(new StoreProduct(R.drawable.red2, "Title 5", "This is description 5", 10));
+        productList.add(new StoreProduct(R.drawable.red2, "Title 6", "This is description 6", 10));
+        productList.add(new StoreProduct(R.drawable.red2, "Title 7", "This is description 7", 10));
+        productList.add(new StoreProduct(R.drawable.red2, "Title 8", "This is description 8", 10));
+        productList.add(new StoreProduct(R.drawable.red2, "Title 9", "This is description 9", 10));
+        productList.add(new StoreProduct(R.drawable.red2, "Title 10", "This is description 10", 10));
+
+        return productList;
+    }
+
+    AdapterView.OnItemClickListener onItemClick = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            //Do any thing when user click to item
+            Toast.makeText(getApplicationContext(), productList.get(position).getTitle() + " - " + productList.get(position).getDescription(), Toast.LENGTH_SHORT).show();
+        }
+    };
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.store_options_bar, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
-    public void onActivityCreated(Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        Log.d("storeFragment","loading the array adapter");
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.item_menu_1:
+                if(VIEW_MODE_LISTVIEW == currentViewMode) {
+                    currentViewMode = VIEW_MODE_GRIDVIEW;
+                } else {
+                    currentViewMode = VIEW_MODE_LISTVIEW;
+                }
+                //Switch view
+                switchView();
+                //Save view mode in share reference
+                SharedPreferences sharedPreferences = getSharedPreferences("ViewMode", MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPreferences.edit();
+                editor.putInt("currentViewMode", currentViewMode);
+                editor.apply();
 
-        /*ListAdapter adapter = ArrayAdapter.createFromResource(getActivity(),
-                R.array.storeitemlist, android.R.layout.simple_list_item_1);
-        setListAdapter(adapter);*/
-
-
-
-        // Construct the data source
-        ArrayList<StoreItem> arrayItems = new ArrayList<StoreItem>();
-// Create the adapter to convert the array to views
-        StoreItemAdapter adapter = new StoreItemAdapter(getActivity(), arrayItems);
-// Attach the adapter to a ListView
-        ListView listView = (ListView) getActivity().findViewById(R.id.StoreItemlist);
-
-        listView.setAdapter(adapter);
-
-        StoreItem newSItem = new StoreItem("Nathan", "San Diego");
-        adapter.add(newSItem);
-
-
-
-        getListView().setOnItemClickListener(this);
+                break;
+        }
+        return true;
     }
+}
 
-    @Override
-    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-        Toast.makeText(getActivity(), "Item: " + position, Toast.LENGTH_SHORT).show();
-
-        // Add item to adapter
-        //StoreItem newSItem = new StoreItem("Nathan", "San Diego");
-        //adapter.add(newUser);
-        /*
-// Or even append an entire new collection
-// Fetching some data, data has now returned
-// If data was JSON, convert to ArrayList of User objects.
-        JSONArray jsonArray = ...;
-        ArrayList<User> newUsers = User.fromJson(jsonArray)
-        adapter.addAll(newUsers);*/
-
-    }
 /*
 // Using an AsyncTask to load the slow images in a background thread
 new AsyncTask<ViewHolder, Void, Bitmap>() {
@@ -121,24 +164,4 @@ new AsyncTask<ViewHolder, Void, Bitmap>() {
         }
     }.execute(holder);
 */
-        // link to get started with listing the store items
-        // https://developer.android.com/guide/topics/ui/layout/listview.html
 
-        //other links:
-        //https://stackoverflow.com/questions/12077955/android-using-simplecursoradapter-to-get-data-from-database-to-listview
-        //https://developer.android.com/reference/android/support/v4/app/ListFragment.html
-        //https://developer.android.com/reference/android/widget/SimpleCursorAdapter.html
-        //https://developer.android.com/reference/android/app/ListFragment.html
-        //https://developer.android.com/guide/topics/ui/declaring-layout.html#AdapterViews
-        //https://developer.android.com/guide/topics/ui/layout/listview.html
-        //https://thinkandroid.wordpress.com/2010/01/09/simplecursoradapters-and-listviews/
-
-/*
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.store_tab_fragment, container, false);
-    }*/
-
-
-}
